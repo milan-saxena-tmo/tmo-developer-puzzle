2f12947b87df23e10bc73e3396f922d9ea24ea8e
Milan Saxena <Milan.Saxena3@T-Mobile.com>
2020-01-15 19:18:17 -0800

    Task 2: typeahead, removed Go button with task -1

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subject } from 'rxjs';
+import { filter, takeUntil, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
 
   quotes$ = this.priceQuery.priceQueries$;
+  private destroyed: Subject<boolean> = new Subject();
 
   timePeriods = [
     { viewValue: 'All available data', value: 'max' },
@@ -22,8 +25,11 @@ export class StocksComponent implements OnInit {
     { viewValue: 'Year-to-date', value: 'ytd' },
     { viewValue: 'Six months', value: '6m' },
     { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
+    { viewValue: 'One month', value: '1m' },
   ];
+
+  chartData: any;
+  isValidForm: boolean;
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
@@ -32,12 +38,35 @@ export class StocksComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.stockPickerForm.valueChanges
+      .pipe(
+        // debounce input for 400 milliseconds
+        debounceTime(400),
+        // only emit if emission is different from previous emission
+        distinctUntilChanged(),
+        // checking if form is valid
+        filter(() => this.stockPickerForm.valid),
+        // unsubscribe
+        takeUntil(this.destroyed))
+      .subscribe((status) => this.fetchQuote());
 
-  fetchQuote() {
-    if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
+    this.quotes$.subscribe(newData => (this.chartData = newData));
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next(true);
+    this.destroyed.complete();
+  }
+
+  private fetchQuote() {
+    const { symbol, period } = this.stockPickerForm.value;
+    if (symbol !== null && symbol !== undefined && symbol.length >= 2) {
       this.priceQuery.fetchQuote(symbol, period);
+      this.isValidForm = true;
+    } else {
+      this.isValidForm = false;
     }
   }
+
 }
