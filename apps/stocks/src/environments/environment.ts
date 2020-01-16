1f4127f5e26f59a8ef7a5a1fc68653d375093dc1
Milan Saxena <Milan.Saxena3@T-Mobile.com>
2020-01-15 19:28:02 -0800

    Task 3: choose custom dates to select records Task 2: typeahead, removed Go button with Task 1: fixed chart display issue

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,7 +6,7 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
+  apiKey: 'Tsk_83a12a88cf5a4b26a6bbfd86a786b43e',
   apiURL: 'https://sandbox.iexapis.com'
 };
 
--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subject } from 'rxjs';
+import { filter, takeUntil, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
+  fromDate: Date;
+  toDate: Date;
+  fromDateFormatted: string;
+  toDateFormatted: string;
 
   quotes$ = this.priceQuery.priceQueries$;
+  private destroyed: Subject<boolean> = new Subject();
 
   timePeriods = [
     { viewValue: 'All available data', value: 'max' },
@@ -22,22 +30,134 @@ export class StocksComponent implements OnInit {
     { viewValue: 'Year-to-date', value: 'ytd' },
     { viewValue: 'Six months', value: '6m' },
     { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
+    { viewValue: 'One month', value: '1m' },
+    { viewValue: 'Custom', value: 'custom' }
   ];
+
+  chartData: any;
+  isValidForm: boolean;
+  isShowDateRange: boolean;
+  maxDate = new Date();
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      period: [null, Validators.required],
+      fromDate: [null, Validators.required],
+      toDate: [null, Validators.required],
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const defaultFromDate: Date = (new Date());
+    defaultFromDate.setMonth(defaultFromDate.getMonth() - 1);
+    const defaultToDate: Date = new Date();
 
-  fetchQuote() {
-    if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
-      this.priceQuery.fetchQuote(symbol, period);
+    this.stockPickerForm.patchValue({
+      fromDate: defaultFromDate,
+      toDate: defaultToDate
+    });
+
+    this.stockPickerForm.valueChanges
+      .pipe(
+        // debounce input for 400 milliseconds
+        debounceTime(400),
+        // only emit if emission is different from previous emission
+        distinctUntilChanged(),
+        // checking if form is valid
+        filter(() => this.stockPickerForm.valid),
+        // unsubscribe
+        takeUntil(this.destroyed))
+      .subscribe((status) => this.fetchQuote());
+
+    this.quotes$.subscribe((newData: any) => {
+
+      if (this.period === 'custom' && newData && newData.length > 0) {
+        this.chartData = newData.filter((x: any) => {
+          return  x[0] >= this.fromDateFormatted &&  x[0] <= this.toDateFormatted
+        });
+      } else {
+        this.chartData = newData;
+      }
+
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next(true);
+    this.destroyed.complete();
+  }
+
+  private fetchQuote() {
+    const { symbol, period, fromDate, toDate } = this.stockPickerForm.value;
+    if (symbol !== null && symbol !== undefined && symbol.length >= 2) {
+      this.fromDateFormatted = moment(fromDate).format('YYYY-MM-DD');
+      this.toDateFormatted = moment(toDate).format('YYYY-MM-DD');
+
+      let range: string = period;
+      if(period === 'custom') {
+        range = this.getRangeFromDateSelection(fromDate);
+      }
+
+      this.period = period;
+
+      this.priceQuery.fetchQuote(symbol, range);
+      this.isValidForm = true;
+    } else {
+      this.isValidForm = false;
     }
+  }
+
+  onPeriodChange(value: any){
+    if (value === 'custom') {
+      this.isShowDateRange = true;
+    } else {
+      this.isShowDateRange = false;
+    }
+  }
+
+  onFromDateChange(e: any) {
+    const { symbol, period, fromDate, toDate } = this.stockPickerForm.value;
+    if(fromDate && toDate && fromDate.getTime() > toDate.getTime()) {
+      this.stockPickerForm.patchValue({
+        toDate: fromDate
+      });
+    }
+    this.fromDate = fromDate;
+  }
+  onToDateChange(e: any) {
+    const { symbol, period, fromDate, toDate } = this.stockPickerForm.value;
+    if(fromDate && toDate && fromDate.getTime() > toDate.getTime()) {
+      this.stockPickerForm.patchValue({
+        fromDate: toDate
+      });
+    }
+    this.toDate = toDate;
+  }
+
+  getRangeFromDateSelection(fromDate: Date) : string {
+    let range = 'max';
+
+    const today = moment(new Date());
+    const startDate = moment(fromDate);
+    const diffInDays = Math.abs(today.diff(startDate, 'days'));
+
+    if(diffInDays > 5 * 365) {
+      range = 'max';
+    } else if(diffInDays > 2 * 365 ) {
+      range = '5y';
+    } else if(diffInDays > 365) {
+      range = '2y';
+    } else if(diffInDays > 6 * 30) {
+      range = '1y';
+    } else if(diffInDays > 3 * 30) {
+      range = '6m';
+    } else if(diffInDays > 30) {
+      range = '3m';
+    } else {
+      range = '1m';
+    }
+
+    return range;
   }
 }
